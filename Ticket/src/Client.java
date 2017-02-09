3e4640dc574b92c1f49c16971c8cd1b31f691c70
DylanLamont <dylan1221@gmail.com>
2017-02-09 09:08:36 -0800

    Day 2: Ticket Done

---
+++ Ticket/src/Client.java
@@ -0,0 +1,7 @@
+
+public class Client {
+	public static void main (String args[]){
+		StudentAdvanceTicket mine = new StudentAdvanceTicket(1, 100);
+		System.out.println(mine.getPrice());
+	}
+}
---
+++ Ticket/src/StudentAdvanceTicket.java
@@ -0,0 +1,11 @@
+
+public class StudentAdvanceTicket extends AdvanceTicket {
+	public StudentAdvanceTicket (int number, int daysUntilEvent){
+		super(number, 0);
+		if (daysUntilEvent>=10){
+			setPrice(15);
+		} else{
+			setPrice(20);
+		}
+	}
+}
