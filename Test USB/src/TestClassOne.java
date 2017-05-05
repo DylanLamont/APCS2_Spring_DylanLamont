1f8fe84f34dd7949cece28b3af8554610fdd3fc0
Dylan Lamont <dlamont1221@gmail.com>
2017-05-05 09:45:50 -0700

    Day 18: Finished importing of JAR files

---
+++ Test USB/src/TestClassOne.java
@@ -0,0 +1,11 @@
+import org.usb4java.javax.*;
+import javax.usb.*;
+import org.usb4java.javax.adapter.*;
+import org.usb4java.javax.descriptors.*;
+import javax.usb.event.*;
+import javax.usb.util.*;
+import java.util.*;
+
+public class TestClassOne {
+
+}
