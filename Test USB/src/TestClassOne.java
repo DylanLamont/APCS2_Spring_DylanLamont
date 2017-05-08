c71012bef005d12e5b21fa5a43d082b58b17945e
Dylan Lamont <dlamont1221@gmail.com>
2017-05-08 09:52:44 -0700

    Work on USB control

--- Test USB/src/TestClassOne.java
+++ Test USB/src/TestClassOne.java
@@ -6,6 +6,31 @@ import javax.usb.event.*;
 import javax.usb.util.*;
 import java.util.*;
 
+
+
 public class TestClassOne {
+	private int VendorID;
+	private int ProductID;
+	
+	public TestClassOne(){
+		VendorID = 0x461;
+		ProductID = 0x4D03;
+	}
+	
+	public UsbDevice findDevice(UsbHub hub, short vendorId, short productId)
+	{
+	    for (UsbDevice device : (List<UsbDevice>) hub.getAttachedUsbDevices())
+	    {
+	        UsbDeviceDescriptor desc = device.getUsbDeviceDescriptor();
+	        if (desc.idVendor() == vendorId && desc.idProduct() == productId) return device;
+	        if (device.isUsbHub())
+	        {
+	            device = findDevice((UsbHub) device, vendorId, productId);
+	            if (device != null) return device;
+	        }
+	    }
+	    return null;
+	}
+
 
 }
